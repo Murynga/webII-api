2bf7a307cfd1d0ec64113ac7a14b8a3a59bca1b0
Walisson Fernandes Dias <wfd@aluno.ifnmg.edu.br>
2026-09-03 16:16:02 -0300

    Atualização do schema do Prisma; Adição de controllers e routes para Subject.

---
+++ src/controllers/subjectController.js
@@ -0,0 +1,150 @@
+// src/controllers/subjectController.js
+import prisma from "../config/database.js";
+
+/**
+ * Controller de Matérias 
+ * Responsável por gerenciar as operações CRUD de matérias
+ */
+
+// CREATE - Criar novo usuário
+export const create = async (req, res) => {
+  try {
+    const { nome, ativa, professorId } = req.body;
+
+    if (!nome || !professorId) {
+      return res.status(400).json({
+        success: false,
+        message: "Nome e Identificador do professor são obrigatórios",
+      });
+    }
+
+    // Cria o usuário no banco
+    const novaMateria = await prisma.subject.create({
+      data: {
+        nome,
+        ativa: ativa || true, //Default: true
+        professorId,
+      },
+      select: {
+        id: true,
+        nome: true,
+        ativa: true,
+        professorId: true,
+        createdAt: true,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Matéria criada com sucesso",
+      data: novaMateria,
+    });
+  } catch (error) {
+    console.error("Erro ao criar matéria:", error);
+
+    // TODO - TESTAR ISSO AQUI!!!
+    if (error.code === "200") { //!!! TÁ ERRADO!!!
+      return res.status(200).json({
+        success: false,
+        message: "ID do professor inexistente!",
+      });
+    }
+
+    res.status(500).json({
+      success: false,
+      message: "Erro ao criar matéria",
+    });
+  }
+};
+
+// READ - Listar todas as matérias
+export const getAll = async (req, res) => {
+  try {
+    const materias = await prisma.subject.findMany({
+      select: {
+        id: true,
+        nome: true,
+        ativa: true,
+        professor_id: {
+          select: {
+          id: true,
+            nome: true,
+            email: true,
+            foto: true,
+            }
+        },
+        createdAt: true,
+      },
+      orderBy: {
+        createdAt: "desc", // Mais recentes primeiro
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      data: materias,
+      total: materias.length,
+    });
+  } catch (error) {
+    console.error("Erro ao listar matérias:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erro ao listar matérias",
+    });
+  }
+};
+
+// READ - Buscar matéria por ID
+export const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Converte string para número
+    const subjectId = Number(id);
+
+    // Validação básica
+    if (!Number.isInteger(subjectId) || subjectId <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "ID inválido. Deve ser um número",
+      });
+    }
+
+    const materia = await prisma.subject.findUnique({
+      where: { id: subjectId },
+      select: {
+        id: true,
+        nome: true,
+        ativa: true,
+        professor_id: {
+          select: {
+          id: true,
+            nome: true,
+            email: true,
+            foto: true,
+            }
+        },
+        createdAt: true,
+      },
+    });
+
+    // Matéria não encontrada
+    if (!materia) {
+      return res.status(404).json({
+        success: false,
+        message: `Matéria com ID${subjectId} não encontrado`,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: materia,
+    });
+  } catch (error) {
+    console.error("Erro ao buscar matéria:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erro ao buscar matéria",
+    });
+  }
+};
